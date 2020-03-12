9e6dbfed1638352e2c4bd71232e3624731e98ea9
ngaivoronski <ngaivoronski@gmail.com>
2020-03-12 18:59:11 -0400

    inventory added to player and rooms. taking, eating, and dropping work. you can now win the game

--- src/adv.py
+++ src/adv.py
@@ -1,27 +1,44 @@
 from room import Room
 from player import Player
+from item import Item
+from item import Food
+
+# list of items
+
+rock = Item("rock", "This is a rock")
+sandwich = Food("sandwich", "club sandwich", 50)
+apple = Food("apple", "granny smith apple", 20)
+key = Item("key", "this key looks secretive")
+silver_coins = Item("silver coins", "a pile of silver coins!")
+gold_coins = Item("gold coins", "a pile of gold coins!")
+sword = Item("sword", "a badass sword")
+painting = Item("painting", "a portrait of... you? Or is it a mirror?")
+bones = Item("bones", "the skeletal remains of another adventurer")
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons", [rock]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", [apple, sandwich]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", [sword]),
 
     "secret_door": Room("Secret Door Room", """Before you stands an empty room with
-a secret door! Perhaps it might unlock with a secret key?"""),
+a secret door! Perhaps it might unlock with a secret key?""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", [key, gold_coins, silver_coins]),
+
+    'enlightenment': Room("Room of enlightenment", """You have found the hidden room of enlightment! 
+    There is no physical escape, only spiritual. Ascend or be trapped forever!""", [painting, bones])
 }
 
 
@@ -66,5 +83,56 @@ while True:
         exit(0)
     elif cmd in valid_directions:
         player.travel(cmd)
+    elif cmd == "i" or cmd == "inventory":
+        player.print_inventory()
+    elif cmd == "l" or cmd == "look":
+        player.look()
+    elif "take" in cmd or "get" in cmd:
+        if len(player.current_room.items) > 0:
+            for item in player.current_room.items:
+                if item.name in cmd:
+                    player.take_item(item)
+        else:
+            print("There are no items in this room to take!")
+    elif "drop" in cmd or "get" in cmd:
+        if len(player.items) > 0:
+            for item in player.items:
+                if item.name in cmd:
+                    player.drop_item(item)
+        else:
+            print("There are no items in your inventory to drop!")
+    elif "eat " in cmd:
+        if len(cmd[4:]) > 0:
+            if len(player.items) > 0:
+                if " " in cmd[4:]:
+                    print("Please specify ONE thing that you would like to eat.")
+                else:
+                    for item in player.items:
+                        if item.name in cmd[4:]:
+                            player.eat(item)
+            else:
+                print("You have no items in your inventory.")
+        else:
+            print("Please specify what you would like to eat.")
+    elif "unlock" in cmd:
+        if player.current_room == room["secret_door"]:
+            if key in player.items:
+                player.unlock(room['enlightenment'])
+            else:
+                print("You don't have the key for this door.")
+        else:
+            print("There is nothing to unlock!")
+    elif "meditate" in cmd or "ascend" in cmd or "pray" in cmd:
+        if player.current_room == room['enlightenment']:
+            if len(player.items) == 0:
+                print("You cast off all of your material possessions and attempt to focus your spirit.")
+                print("The heavens open and you leave behind your physical body as you transcend into a higher plain of existence.")
+                print("You feel at one with all things. You have achieved true enlightenment! Congratulations!")
+                print("Then end!")
+                exit(0)
+            else:
+                print("You try to ascend... but to no affect!")
+        else:
+            print("Now is not the time for that!")
     else:
         print("I did not understand that command")
--- src/player.py
+++ src/player.py
@@ -1,12 +1,50 @@
+from item import Item
+from item import Food
+
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-class Player():
+class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.items = []
     def travel(self, direction):
         if getattr(self.current_room, f"{direction}_to"):
             self.current_room = getattr(self.current_room, f"{direction}_to")
             print(self.current_room)
         else:
             print("You cannot move in that direction")
+    def print_inventory(self):
+        if len(self.items) > 0:
+            print("You are holding: ")
+            for item in self.items:
+                print(f'{item.name}: {item.description}')
+        else:
+            print("You have no items.")
+    def eat(self, food_item):
+        if isinstance(food_item, Food):
+            print(f"You have eaten {food_item.name}")
+            self.items.remove(food_item)
+        else:
+            print("You cannot eat that item!")
+        
+    def look(self):
+        print(f"You are in {self.current_room.name}\n")
+        print(f"{self.current_room.description}\n")
+        if len(self.current_room.items) > 0:
+            print("You see the following items:")
+            for item in self.current_room.items:
+                print(item.name)
+    def take_item(self, taken_item):
+        self.current_room.items.remove(taken_item)
+        self.items.append(taken_item)
+        print(f"You took the {taken_item.name}")
+    def drop_item(self, dropped_item):
+        self.current_room.items.append(dropped_item)
+        self.items.remove(dropped_item)
+        print(f"You dropped the {dropped_item.name}")
+    def unlock(self, unlocked_room):
+        self.current_room = unlocked_room
+        print("You have used your key to unlock the door!")
+        print("You step through but the door shuts behind you!")
+        print(self.current_room)
